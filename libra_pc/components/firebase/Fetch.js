6042f2cce250370ea2c04b6ca0aecd4f372740bf
neeraj@pairoxz <neeraj@pairroxz.com>
2022-12-23 19:44:31 +0530

    updated fetc

--- libra_pc/components/firebase/Fetch.js
+++ libra_pc/components/firebase/Fetch.js
@@ -1,26 +1,52 @@
-import React, { useEffect } from "react";
-import {View,Text} from 'react-native';
+import React, { useEffect, useState } from "react";
+import {View,Text, StatusBar} from 'react-native';
 import firestore from '@react-native-firebase/firestore';
+import database from '@react-native-firebase/database';
 
 const Fetch=()=>{
+    const [MyData,SetMyData] = useState(null);
+    const[RealData,SetMyRealData] = useState(null);
     useEffect(()=>{
         getData();
+        getRealTimeData();
     },[])
 
+    //firestore data getting
     const getData=async()=>{
         try {
         
         const data = await firestore().collection('testing').doc('0LKKYVUU2CVCJI6QJ3Z4').get();
-        console.log(data);
+        // console.log(data);
+        SetMyData(data._data);
         } catch (error) {
             console.log('====================================');
             console.log(error);
             console.log('====================================');
         }
     }
+
+    //realtime data get
+const getRealTimeData =async()=>{
+    try {
+        const data = await database().ref('Users/1').once('value');
+        console.log("Realtime data",data);
+        
+    } catch (error) {
+        console.log(error);
+    }
+}
     return(
-        <View style={{justifyContent:'center',alignContent:'center',flex:1}}>
-            <Text style={{alignSelf:'center'}}>Firebase Data</Text>
+        <View style={{flex:1}}>
+           {/* <StatusBar hidden/> */}
+            <Text style={{alignSelf:'center'}}>Name:{MyData?MyData.name:"Loading..."}</Text>
+            <Text style={{alignSelf:'center'}}>Age:{MyData?MyData.age:"Loading..."}</Text>
+            <Text style={{alignSelf:'center'}}>Fruits:{MyData?MyData.fruits.map(l => ` ${l},`):"Loading..."}</Text>
+            <View style={{borderBottomWidth:1,  borderBottomColor:'black', width:'100%', marginTop:2}}></View>
+            <Text style={{fontSize:20, fontWeight:'bold', alignSelf:'center', color:'red'}}>RealTime data</Text>
+
+            
+
+            
         </View>
     )
 
